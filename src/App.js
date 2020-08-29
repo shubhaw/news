e5812d19d09ba9ab34069c9d5f1c12e8e2c12e29
Shubhaw Kumar <dev.shubhaw@gmail.com>
2020-08-29 15:53:07 +0530

    completed initial setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
-
+import axios from "../src/axios-config";
 import './App.css';
 
-class App extends React.Component {
-  render() {
-    return <h1>News App</h1>
-  }
+const App = () => {
+    React.useEffect(() => {
+        // axios.get("top-headlines?country=us")
+        //     .then(response => console.log(response))
+        //     .catch(err => console.log(err));
+    }, []);
+    return <h1>News App - Developed by Shubhaw</h1>
 }
-
 
 export default App;
